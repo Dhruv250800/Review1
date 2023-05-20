f3e1d1c19356a9d46f5216b8d02e6384dffbc183
DhruvGour <dhruv250800@gmail.com>
2023-05-20 08:30:53 +0530

    [DhruvGour]PracticeQuestion1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int m, sum = 0;
+Console.WriteLine("Hello User!");
+Console.Write("Please Enter a Number : ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n > 0)
+{
+    m = n % 10;
+    sum = sum + m;
+    n = n / 10;
+}
+Console.WriteLine("Sum of the digits of the given number is : " + sum);
